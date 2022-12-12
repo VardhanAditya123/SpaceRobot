ca85ecde7559e47a5044c04ac936f019fc68083a
Aditya Vardhan <vardhan@adityas-air.attlocal.net>
2022-12-11 21:54:14 -0500

    Increased Variation perfect play

--- DualArm/dualArm.py
+++ DualArm/dualArm.py
@@ -39,7 +39,7 @@ aoptimizer = Adam(anetwork.parameters(), lr=pi_lr)
 qoptimizer = Adam(qnetwork.parameters(), lr=q_lr)
 
 def get_action(o, noise_scale):
-    noise_scale = 0.15
+    noise_scale = 0.25
     act_limit = env.action_space.high[0]
     act_dim = env.action_space.shape[0]
     a = anetwork(torch.as_tensor(o, dtype=torch.float32)).detach().numpy()
@@ -66,10 +66,10 @@ def compute_loss_q(data):
 def compute_loss_pi(data):
     o = data['obs']
     a = data['act']
-    # var_noise = 0.3
-    # q_pi = (qnetwork(o, anetwork(o))) - (var_noise*torch.var(a,dim=1))
-    var_noise = 0.2
-    q_pi = (qnetwork(o, anetwork(o))) + (var_noise*torch.var(a,dim=1))
+    var_noise = 0.25
+    q_pi = (qnetwork(o, anetwork(o))) - (var_noise*torch.var(a,dim=1))
+    # var_noise = 0.2
+    # q_pi = (qnetwork(o, anetwork(o))) + (var_noise*torch.var(a,dim=1))
     # print(q_pi,torch.var(a,dim=1))
     return -q_pi.mean()
 
@@ -155,9 +155,6 @@ def main():
                 update(replay_buffer, batch_size)
                 update(replay_buffer, batch_size)
                 update(replay_buffer, batch_size)
-                update(replay_buffer, batch_size)
-
-              
                          
                 
                 
